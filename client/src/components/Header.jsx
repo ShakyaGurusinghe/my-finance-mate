50f18f1d3ac286174fb4db16e68a811009df663f
ShakyaGurusinghe <shakyapersonal1@gmail.com>
2025-03-23 12:11:47 +0530

    Revert "NavBar color theme modification"
    This reverts commit 8a14ec920b4a275331771cdebd23b4b022af7723.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-custom-purple shadow-md">
+    <header className="bg-emerald-100 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         {/* Logo */}
         <Link to="/" className="flex items-center">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-custom-purple">MyFinance</span>
+            <span className="text-emerald-500">MyFinance</span>
             <span className="text-slate-700">Mate</span>
           </h1>
         </Link>
@@ -21,11 +21,11 @@ export default function Header() {
             aria-label="Search"
             className="focus:outline-none bg-transparent w-32 sm:w-64 px-2 text-slate-700"
           />
-          <FaSearch className="text-custom-navBar-purple ml-2" />
+          <FaSearch className="text-emerald-600 ml-2" />
         </form>
 
         {/* Navigation Links */}
-        <ul className="flex space-x-4 text-sm sm:text-base text-custom-navBar-purple items-center">
+        <ul className="flex space-x-4 text-sm sm:text-base text-emerald-700 items-center">
           <li className="hidden sm:inline hover:text-emerald-900 hover:underline cursor-pointer">
             <Link to="/">Home</Link>
           </li>
@@ -34,7 +34,7 @@ export default function Header() {
           </li>
           <li>
             <Link to="/signin">
-              <button className="bg-custom-purple-button text-white px-4 py-1 rounded-lg hover:bg-emerald-700 transition">
+              <button className="bg-emerald-500 text-white px-4 py-1 rounded-lg hover:bg-emerald-700 transition">
                 Sign in
               </button>
             </Link>
