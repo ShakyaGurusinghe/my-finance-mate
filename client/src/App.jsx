4e1bf098a16db3598fed30b451000096d545c310
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-04-04 15:43:08 +0530

    refactor: remove Header component and its usage in App for cleaner structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import Header from "./components/Header";
 import Budget from "./pages/Budget";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
