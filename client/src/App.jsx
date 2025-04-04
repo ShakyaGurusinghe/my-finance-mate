940c142d026a9d24a02d086f0f2dfa527867081d
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-04-04 15:41:45 +0530

    refactor: simplify App component structure by removing unnecessary AppContent function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,26 @@
-import { BrowserRouter, Routes, Route, useLocation, } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-
-import Dashboard from "./pages/Dashboard";
-import Sidebar from "./components/Sidebar";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 import Budget from "./pages/Budget";
-
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AppContent />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </BrowserRouter>
-  );
-}
-
-function AppContent() {
-  const location = useLocation(); // Get the current route
-
-  return (
-    <div className="flex">
-     
-{(location.pathname === "/dashboard" || location.pathname === "/budget" ) && <Sidebar />}
-
-
-      <div className="flex-1">
-        
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/budget" element={<Budget />} />
-
-        </Routes>
-      </div>
-    </div>
   );
 }
